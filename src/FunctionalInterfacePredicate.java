8d6092a1be4cc5b6afde47c8dc22b906b9d6f5d4
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-06 21:42:59 -0300

    Testing the use of the functional interface predicate - way 2

--- src/FunctionalInterfacePredicate.java
+++ src/FunctionalInterfacePredicate.java
@@ -5,6 +5,8 @@ import java.util.function.Predicate;
 public class FunctionalInterfacePredicate {
     public static void main(String[] args) throws Exception {
         FunctionalInterfacePredicate.forma1();
+        System.out.println("------------------------");
+        FunctionalInterfacePredicate.forma2();
     }
 
     public static void forma1() {
@@ -14,5 +16,20 @@ public class FunctionalInterfacePredicate {
 
         linguagensProgramacao.stream().filter(maisDeCincoLetras).forEach(System.out::println);
 
+    }
+
+    public static void forma2() {
+        List<String> linguagensProgramacao = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
+
+        linguagensProgramacao
+            .stream()
+            .filter(new Predicate<String>() {
+                @Override
+                public boolean test(String palavra) {
+                    return palavra.length() > 5;
+                }
+                
+            })
+            .forEach(System.out::println);
     }
 }
