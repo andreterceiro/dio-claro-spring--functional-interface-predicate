3c862e5b8518191a639d1a0681ddcd767b2caec0
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-06 21:54:26 -0300

    Adding JavaDoc

--- src/FunctionalInterfacePredicate.java
+++ src/FunctionalInterfacePredicate.java
@@ -2,7 +2,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
+/**
+ * Class to make tests related to the functional interface Predicate
+ */
 public class FunctionalInterfacePredicate {
+    /**
+     * Main method to execute the tests invoking other methods
+     *
+     * @param args CLI args (ignored)
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
         FunctionalInterfacePredicate.forma1();
         System.out.println("------------------------");
@@ -11,6 +20,9 @@ public class FunctionalInterfacePredicate {
         FunctionalInterfacePredicate.forma3();
     }
 
+    /**
+     * Way 1 to use the functional interface Predicate
+     */
     public static void forma1() {
         List<String> linguagensProgramacao = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
@@ -19,6 +31,9 @@ public class FunctionalInterfacePredicate {
         linguagensProgramacao.stream().filter(maisDeCincoLetras).forEach(System.out::println);
     }
 
+    /**
+     * Way 2 to use the functional interface Predicate
+     */
     public static void forma2() {
         List<String> linguagensProgramacao = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
@@ -34,6 +49,9 @@ public class FunctionalInterfacePredicate {
             .forEach(System.out::println);
     }
 
+    /**
+     * Way 3 to use the functional interface Predicate
+     */
     public static void forma3() {
         List<String> linguagensProgramacao = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
